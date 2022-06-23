41191c29ea2b048b876c3f3a2d4e4088e9062ebd
Ayeshanwl <ayeshawahla77@gmail.com>
2022-06-23 04:29:36 -0700

    practice

---
+++ practice program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+// first program of sum
+	  /*int a, b, sum;
+	  cout << "Enter the value of A ";
+	  cin >> a;
+	  cout << "Enter the value of b ";
+	  cin >> b;
+	  sum = a+b;
+	  cout << " Sum of a and b: " << sum <<endl;*/
+}
