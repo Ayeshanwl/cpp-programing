23eeca49257f807edb4267e50d89cd10ef4672dc
Ayeshanwl <ayeshawahla77@gmail.com>
2022-06-23 04:12:12 -0700

    cpp file

---
+++ day2.cpp
@@ -0,0 +1,19 @@
+
+
+void main()
+
+{
+   char name;
+   char gender;
+   int age;
+   float marks;
+   float height;
+   
+   
+   name="A";
+   gender="female";
+   age=20;
+   marks=80.43%;
+   height=5.2;
+    
+}
